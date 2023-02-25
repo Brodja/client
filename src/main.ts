6afe59ab7097ae757fa525df29fd81d9b47501e3
Brodja <body1906@gmail.com>
2023-02-25 09:21:47 +0200

    add materialize

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import 'materialize-css/dist/js/materialize.js';
+//\ enableProdMode();
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
