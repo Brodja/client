111623b6468122b1c80d328caa6ac5b8403456ce
Bohdan <body1906@gmail.com>
2023-11-19 12:59:54 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './shared/services/auth.service';
 import { MaterialService } from './shared/classes/material.service';
 import { Subscription } from 'rxjs';
@@ -8,31 +8,13 @@ import { Router } from '@angular/router';
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent implements OnInit, OnDestroy {
-  uSub!: Subscription;
+export class AppComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
-
-  ngOnDestroy() {
-    if (this.uSub) this.uSub.unsubscribe();
-  }
 
   ngOnInit(): void {
     const potentialToken = localStorage.getItem('auth-token');
     if (potentialToken) {
       this.authService.setToken(potentialToken);
-      this.uSub = this.authService.initUser().subscribe(
-        (user) => {
-          this.authService.setUser(user);
-          if (user?.currentGame) {
-            if (this.router.url !== user?.currentGame?.type) {
-              this.router.navigate([`../${user?.currentGame?.type}`]);
-            }
-          }
-        },
-        (error) => {
-          MaterialService.toast(error.error.message);
-        }
-      );
     }
   }
 }
--- src/app/shared/layouts/site-layout/site-layout.component.ts
+++ src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { SocketService } from '../../services/socket.service';
+import { Subscription } from 'rxjs';
+import { MaterialService } from '../../classes/material.service';
 
 @Component({
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
   styleUrls: ['./site-layout.component.css'],
 })
-export class SiteLayoutComponent {
+export class SiteLayoutComponent implements OnInit, OnDestroy {
+  uSub!: Subscription;
+
   links = [
     { url: '/news', name: 'Новини' },
     { url: '/profile', name: 'Профіль' },
@@ -16,11 +20,39 @@ export class SiteLayoutComponent {
     { url: '/rooms', name: 'Кімнати' },
   ];
 
-  constructor(private auth: AuthService, private router: Router,  private socketService: SocketService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private socketService: SocketService
+  ) {}
+
+  ngOnDestroy() {
+    if (this.uSub) this.uSub.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    const potentialToken = localStorage.getItem('auth-token');
+    if (potentialToken) {
+      this.authService.setToken(potentialToken);
+      this.uSub = this.authService.initUser().subscribe(
+        (user) => {
+          this.authService.setUser(user);
+          if (user?.currentGame) {
+            if (this.router.url !== user?.currentGame?.type) {
+              this.router.navigate([`../${user?.currentGame?.type}`]);
+            }
+          }
+        },
+        (error) => {
+          MaterialService.toast(error.error.message);
+        }
+      );
+    }
+  }
 
   logout(event: Event) {
     event.preventDefault();
-    this.auth.logout();
+    this.authService.logout();
     this.router.navigate(['/login']);
   }
 }
